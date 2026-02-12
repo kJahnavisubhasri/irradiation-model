378fb261ac3ff6cf58efedcee1c571c6bcd58f8c
kJahnavisubhasri <subhasri90303@gmail.com>
2026-02-12 09:51:02 +0530

    Configure Flask app to run on specified host and port
    Updated the app to run on a specified port and host.

--- app.py
+++ app.py
@@ -55,6 +55,10 @@ def predict():
         return jsonify({"error": str(e)})
     
 
+
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
